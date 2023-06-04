bf865443e2145831a83ad38f2467e74a6b65391e
aditya-thakur <adityathakur532@gmail.com>
2023-06-04 08:10:44 +0530

    todo completed

--- TODO-App/scripts/todo.js
+++ TODO-App/scripts/todo.js
@@ -2,9 +2,13 @@ let todoItems = ["Find Socks", "Get Dressed", "Bring Flowers"];
 let doneItems = ["Buy Mangoes", "Buy Litchi"];
 refresh();
 
-function addTask(){
+function addTask() {
     let task = document.getElementById("taskInput");
-    todoItems.push(task.value);
+    if (task.value != "") {
+        todoItems.push(task.value);
+    } else {
+        alert("Please enter a task to add.");
+    }
     task.value = "";
     refresh();
 }
@@ -16,22 +20,22 @@ function refresh() {
     //     const element = todoItem[i];
     //     todoElement.innerHTML += "<li>" + element + "</li>";
     // }
-    todoItems.forEach( (todoItem, index) => {
-        todoElement.innerHTML += `<li title="Click to move to done" class="item" onclick="moveToDone(${index})" >` + todoItem + "</li>";
+    todoItems.forEach((todoItem, index) => {
+        todoElement.innerHTML += `<li title="Click to move to done" class="item" onclick="moveToDone(${index})" ><div>` + todoItem + `</div><div class="crossButton" onclick='removeFromDo(${index})'>X</div></li>`;
     });
 
     let doneElement = document.getElementById("doneItem");
     doneElement.innerHTML = "";
 
     doneItems.forEach((doneItem, index) => {
-        doneElement.innerHTML += `<li title="Click to move to do" class="item" onclick="moveToDo(${index})">` + doneItem + "</li>";
+        doneElement.innerHTML += `<li title="Click to move to do" class="item" onclick="moveToDo(${index})"><div>` + doneItem + `</div><div class="crossButton" onclick='removeFromDone(${index})'>X</div></li>`;
     });
 }
 
 function moveToDo(id) {
     let newList = [];
-    doneItems.forEach( (doneItem, index) => {
-        if(index != id) {
+    doneItems.forEach((doneItem, index) => {
+        if (index != id) {
             newList.push(doneItem);
         } else {
             todoItems.push(doneItem);
@@ -43,13 +47,35 @@ function moveToDo(id) {
 
 function moveToDone(id) {
     let newList = [];
-    todoItems.forEach( (todoItem, index) => {
-        if(index != id) {
+    todoItems.forEach((todoItem, index) => {
+        if (index != id) {
             newList.push(todoItem);
         } else {
             doneItems.push(todoItem);
         }
     });
     todoItems = newList;
+    refresh();
+}
+
+function removeFromDo(id) {
+    let newList = [];
+    todoItems.forEach((todoItem, index) => {
+        if (index != id) {
+            newList.push(todoItem);
+        }
+    });
+    todoItems = newList;
+    refresh();
+}
+
+function removeFromDone(id) {
+    let newList = [];
+    doneItems.forEach((doneItem, index) => {
+        if (index != id) {
+            newList.push(doneItem);
+        }
+    });
+    doneItems = newList;
     refresh();
 }
